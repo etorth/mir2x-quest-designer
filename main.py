68e165231822095548fa69db1be56e26c8774461
anhong <anhonghe@gmail.com>
2022-07-16 19:29:50 -0700

    f

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6 import QtWidgets, uic
 
+app = QtWidgets.QApplication(sys.argv)
 
-def main():
-
-    app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 200)
-    w.move(300, 300)
-
-    w.setWindowTitle('Simple')
-    w.show()
-
-    sys.exit(app.exec())
-
-
-if __name__ == '__main__':
-    main()
+window = uic.loadUi("mainwindow.ui")
+window.show()
+app.exec()
